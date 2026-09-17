db8a526e0759cbed96d7bd8fa302c69cb63222be
Varun Jhaveri <varun.jhaveri.02@gmail.com>
2026-09-17 01:00:57 -0400

    Implement Baa rate lookup and CSV parsing

--- Baa_Rate_Analyzer/homework_1.cpp
+++ Baa_Rate_Analyzer/homework_1.cpp
@@ -7,37 +7,124 @@ using namespace std;
 
 double average(vector<double> v)
 {
-// code for calculating average of members of v
-// and returning the average
+    if (v.empty())
+    {
+        return 0.0;
+    }
+
+    double total = 0.0;
+
+    for (double x : v)
+    {
+        total += x;
+    }
+
+    return total / v.size();
 }
 
 double find_rate(vector<double> rate_vec, vector<string> date_vec, string date)
 {
-// code for finding the Baa rate for the
-// given date (in yyyy-mm format)
-// and returning the rate for that month
+    for (size_t i = 0; i < date_vec.size() && i < rate_vec.size(); i++)
+    {
+        if (date_vec[i] == date)
+        {
+            return rate_vec[i];
+        }
+    }
+
+    return -1.0;
 }
 
 int main()
 {
     vector<double> rate;
     vector<string> date;
+
     ifstream infile("./hw1_H.15_Baa_Data.csv");
 
-// code for loading rate and date vectors from the file hw1_H.15_Baa_Data.csv
-// the headers should be handled properly. do not delete them manually
+    // Make sure the file opened successfully
+    if (!infile.is_open())
+    {
+        cout << "Error: could not open hw1_H.15_Baa_Data.csv" << endl;
+        return 1;
+    }
 
+    string line;
 
-   infile.close();
+    // Skip all metadata/header rows until "Time Period"
+    while (getline(infile, line))
+    {
+        if (line.find("Time Period") != string::npos)
+        {
+            break;
+        }
+    }
 
-// code for prompting user for a date and returning the rate
-// and the difference between the rate for that date and the
-// average rate
-//
-// This code should allow the user to continue to input dates
-// until the user inputs the EOF (End-of-file), namely control-d (in linux/mac) or control-z (in windows)
-// This should not crash if a bad date is entered.
+    // Load dates and rates
+    while (getline(infile, line))
+    {
+        if (line.empty())
+        {
+            continue;
+        }
 
-   return 0.0; // program end
+        size_t comma = line.find(',');
+
+        if (comma == string::npos)
+        {
+            continue;
+        }
+
+        string date_value = line.substr(0, comma);
+        string rate_value = line.substr(comma + 1);
+
+        try
+        {
+            double rate_number = stod(rate_value);
+
+            date.push_back(date_value);
+            rate.push_back(rate_number);
+        }
+        catch (...)
+        {
+            // Ignore malformed data rows rather than crashing
+            continue;
+        }
+    }
+
+    infile.close();
+
+    // Make sure data was actually loaded
+    if (rate.empty() || date.empty())
+    {
+        cout << "Error: no data was loaded from the file." << endl;
+        return 1;
+    }
+
+    double avg_rate = average(rate);
+
+    string user_date;
+
+    cout << "Enter a date in yyyy-mm format (Ctrl-D to exit):" << endl;
+
+    while (cin >> user_date)
+    {
+        double current_rate = find_rate(rate, date, user_date);
+
+        if (current_rate == -1.0)
+        {
+            cout << "The input date is not in the data." << endl;
+        }
+        else
+        {
+            cout << "Rate: " << current_rate << endl;
+
+            cout << "Difference from average: "
+                 << current_rate - avg_rate << endl;
+        }
+
+        cout << "Enter another date (Ctrl-D to exit):" << endl;
+    }
+
+    return 0;
 }
-
